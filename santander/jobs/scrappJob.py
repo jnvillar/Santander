f46a5c560b292698668f1166b67b7938500e1072
jnvillar <juan.villar@mercadolibre.com>
2017-09-22 09:47:59 -0300

    cron

--- santander/jobs/scrappJob.py
+++ santander/jobs/scrappJob.py
@@ -7,23 +7,8 @@ scrapper_tool = scrapper.Scrapper()
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='tue-sat', hour=4)
+@sched.scheduled_job('cron', day_of_week='2-5', hour=4)
 def scrapp():
-    print('Scrapping')
-
-    investments_founds_names = scrapper_tool.get_investment_founds()
-    investments_founds_values = scrapper_tool.get_investment_founds_today_values()
-    investments_founds_values = investments_founds_values[1::5]
-
-    print('Saving data')
-
-    database.insert_today_values(investments_founds_names, investments_founds_values)
-
-    print('Data saved')
-
-
-@sched.scheduled_job('interval', minutes=1)
-def interval():
     print('Scrapping')
 
     investments_founds_names = scrapper_tool.get_investment_founds()
