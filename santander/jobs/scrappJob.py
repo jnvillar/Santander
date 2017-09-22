ac060a5667fa41eb3ef26d01ab3abf1be7f49319
jnvillar <juan.villar@mercadolibre.com>
2017-09-22 09:39:06 -0300

    cron

--- santander/jobs/scrappJob.py
+++ santander/jobs/scrappJob.py
@@ -21,4 +21,19 @@ def scrapp():
 
     print('Data saved')
 
+
+@sched.scheduled_job('interval', minutes=1)
+def interval():
+    print('Scrapping')
+
+    investments_founds_names = scrapper_tool.get_investment_founds()
+    investments_founds_values = scrapper_tool.get_investment_founds_today_values()
+    investments_founds_values = investments_founds_values[1::5]
+
+    print('Saving data')
+
+    database.insert_today_values(investments_founds_names, investments_founds_values)
+
+    print('Data saved')
+
 sched.start()
