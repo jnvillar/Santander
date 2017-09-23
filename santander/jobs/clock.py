78264e02e8e54a3c85b9262b2ba5cf42f9eadc9f
jnvillar <juan.villar@mercadolibre.com>
2017-09-23 17:34:32 -0300

    cron changed to interval

--- santander/jobs/clock.py
+++ santander/jobs/clock.py
@@ -2,23 +2,27 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 from santander.helpers import scrapper
 from santander.services import database
+import time
 
 scrapper_tool = scrapper.Scrapper()
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='2-5', hour=4)
+@sched.scheduled_job('interval', hours=24)
 def scrapp():
     print('Scrapping')
+    day = time.strftime("%a")
+    
+    if day is not 'Sun' and day is not 'Mon':
+        investments_founds_names = scrapper_tool.get_investment_founds()
+        investments_founds_values = scrapper_tool.get_investment_founds_today_values()
+        investments_founds_values = investments_founds_values[1::5]
 
-    investments_founds_names = scrapper_tool.get_investment_founds()
-    investments_founds_values = scrapper_tool.get_investment_founds_today_values()
-    investments_founds_values = investments_founds_values[1::5]
+        print('Saving data')
 
-    print('Saving data')
+        database.insert_today_values(investments_founds_names, investments_founds_values)
 
-    database.insert_today_values(investments_founds_names, investments_founds_values)
+        print('Data saved')
 
-    print('Data saved')
 
 sched.start()
