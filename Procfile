bb360bd9277d3b2ea2a7988a35e88e562cd9d368
jnvillar <juan.villar@mercadolibre.com>
2017-09-27 09:49:05 -0300

    chau cron

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 web: gunicorn application:app
 init: python db_create.py
 upgrade: python db_upgrade.py
-clock: python santander/jobs/clock.py --loglevel=INFO
--- santander/jobs/clock.py
+++ santander/jobs/clock.py
@@ -1,28 +1,19 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
 from santander.helpers import scrapper
 from santander.services import database
 import time
 
 scrapper_tool = scrapper.Scrapper()
-sched = BlockingScheduler()
 
+print('Scrapping')
+day = time.strftime("%a")
 
-@sched.scheduled_job('interval', hours=1)
-def scrapp():
-    print('Scrapping')
-    day = time.strftime("%a")
-    
-    if day is not 'Sun' and day is not 'Mon':
-        investments_founds_names = scrapper_tool.get_investment_founds()
-        investments_founds_values = scrapper_tool.get_investment_founds_today_values()
-        investments_founds_values = investments_founds_values[1::5]
+if day is not 'Sun' and day is not 'Mon':
+    investments_founds_names = scrapper_tool.get_investment_founds()
+    investments_founds_values = scrapper_tool.get_investment_founds_today_values()
+    investments_founds_values = investments_founds_values[1::5]
 
-        print('Saving data')
+    print('Saving data')
 
-        database.insert_today_values(investments_founds_names, investments_founds_values)
+    database.insert_today_values(investments_founds_names, investments_founds_values)
 
-        print('Data saved')
-
-
-sched.start()
+    print('Data saved')
